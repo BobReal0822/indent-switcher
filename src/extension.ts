4dd559cde2d695387e7f5ae1ff580aadac152c9a
ephoton <i@ephoton.me>
2017-10-18 21:23:24 +0800

    feat: add command si42 & si24

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,55 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// Import the necessary extensibility types to use in your code below
+import { commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, window} from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "indent-switcher" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+interface ISwitchOptions {
+    from: number;
+    to: number;
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+const SwithFrom4To2: ISwitchOptions = {
+    from: 4,
+    to: 2
+};
+
+const SwitchFrom2To4: ISwitchOptions = {
+    from: 2,
+    to: 4
+};
+
+// This method is called when your extension is activated. Activation is
+// controlled by the activation events defined in package.json.
+export function activate(context: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error).
+    // This line of code will only be executed once when your extension is activated.
+    console.log('Congratulations, your extension "WordCount" is now active!');
+
+    // create a new word counter
+    const indentManager = new IndentManager();
+
+    const si42 = commands.registerCommand('extension.si42', () => {
+        // indentManager.switchIndent(SwithFrom4To2);
+        window.showInformationMessage('si42 (indentSwitcher)');
+    });
+    const si24 = commands.registerCommand('extension.si24', () => {
+        // indentManager.switchIndent(SwithFrom4To2);
+        window.showInformationMessage('si24 (indentSwitcher)');
+    });
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(si42);
+    context.subscriptions.push(si24);
+}
+
+class IndentManager {
+
+    public getIndent() {
+        //
+    }
+
+    public switchIndent(options: ISwitchOptions) {
+        //
+
+    }
 }
